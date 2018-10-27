7d839e4ca4539ce384f1ec3bc7646fbb2d0a3635
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2018-10-27 16:02:50 +0300

    Algorithm [hack]: add binarySearch

---
+++ binarySearch.js
@@ -0,0 +1,28 @@
+function binarySearch(array, value) {
+  const arrayLength = array.length;
+
+  if (arrayLength === 1) {
+    return array[0] === value;
+  }
+
+  const middlePoint = Math.floor(array.length / 2);
+
+  if (array[middlePoint] === value) {
+    return true;
+  } else if (array[middlePoint] > value) {
+    let newArray = getPartMassive(array, middlePoint, 'left')
+    return binarySearch(newArray, value)
+  } else {
+    let newArray = getPartMassive(array, middlePoint, 'right')
+    return binarySearch(newArray, value)
+  }
+}
+
+function getPartMassive(array, index, direction) {
+  if (direction === 'right') {
+    return array.slice(index + 1);
+  } else {
+    return array.slice(0, index);
+  }
+}
+
