60468c49975f1df3f899f8388f63429a5d1c05ce
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2018-10-27 14:33:29 +0300

    Algorithm [hack]: bubble sort add exit

--- bubbleSort.js
+++ bubbleSort.js
@@ -2,13 +2,16 @@ function bubbleSort(array) {
   let cashValue;
 
   for (let firstElementIndex = 0; firstElementIndex < array.length; firstElementIndex++) {
+    let isSorted = true;
     for (let secondElementIndex = 0; secondElementIndex < array.length; secondElementIndex++) {
       if (array[firstElementIndex] < array[secondElementIndex]) {
         cashValue = array[firstElementIndex];
         array[firstElementIndex] = array[secondElementIndex];
         array[secondElementIndex] = cashValue;
+        isSorted = false;
       }
     }
+    if (isSorted) break;
   }
 
   return array;
