59ae453c814d96aff7ff4fb35f38d0c9d9a0d77a
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2018-10-27 17:27:32 +0300

    Algorithm [hack]: binarySearch not recursive without slice

--- binarySearch.js
+++ binarySearch.js
@@ -30,29 +30,22 @@ function getPartMassive(array, index, direction) {
 
 function binarySearch(array, value) {
   const arrayLength = array.length;
-  let cloneArray = array.slice();
   let result = false;
-  let middlePoint = Math.floor(cloneArray.length / 2);
+  let starIndex = 0;
+  let endIndex = array.length - 1;
+  let middlePoint = Math.floor((starIndex + endIndex) / 2);
 
   if (arrayLength === 0) return false;
 
-  while (!result && cloneArray.length > 0) {
-    if (cloneArray[middlePoint] === value) {
+  while (!result && starIndex <= endIndex) {
+    if (array[middlePoint] === value) {
       result = true;
-    } else if (cloneArray[middlePoint] > value) {
-      cloneArray = getPartMassive(cloneArray, middlePoint, 'left')
-    } else if (cloneArray[middlePoint] < value) {
-      cloneArray = getPartMassive(cloneArray, middlePoint, 'right')
+    } else if (array[middlePoint] > value) {
+      endIndex = middlePoint - 1;
+    } else if (array[middlePoint] < value) {
+      starIndex = middlePoint + 1;
     }
-    middlePoint = Math.floor(cloneArray.length / 2);
+    middlePoint = Math.floor((starIndex + endIndex) / 2);
   }
   return result;
-}
-
-function getPartMassive(array, index, direction) {
-  if (direction === 'right') {
-    return array.slice(index + 1);
-  } else {
-    return array.slice(0, index);
-  }
 }
